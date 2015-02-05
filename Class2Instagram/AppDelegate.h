e0e28fea0f3151b6cca23f3022d659491b1e9593
AP Fritts <ap@apfritts.com>
2015-02-04 19:33:36 -0800

    Initial Commit

---
+++ Class2Instagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Class2Instagram
+//
+//  Created by AP Fritts on 2/4/15.
+//  Copyright (c) 2015 AP Fritts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Class2Instagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Class2Instagram
+//
+//  Created by AP Fritts on 2/4/15.
+//  Copyright (c) 2015 AP Fritts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
